2ebfc2fd9b36d533a65c1f9b3072e2581a257393
unknown <malibu1106@gmail.com>
2024-04-17 22:31:03 +0200

    Fonctionnement ok en tour par tour

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
 // VARIABLES
 let colors = ['red', 'green', 'yellow', 'blue']; // liste des couleurs
-let colorSuit = Array(); // init tableau
-
+let colorList = new Array(); // init tableau
 // FUNCTIONS
 function getRandomInt() { // on fait une fonction pour tirer un nombre aléatoire
     return Math.floor(Math.random() * 4); // entre 0 et 3
 }
-function colorSuiteChooser() { // on fait une fonction pour remplir le tableau colorSuite > listes des couleurs à deviner
-    for (let i = 0; i < 10; i++) {
-        colorSuit[i] = colors[getRandomInt()]; // Ligne [i] = color random
-
+let i = 0;
+function nextColorPicker() { // on fait une fonction pour remplir le tableau colorSuite > listes des couleurs à deviner
+    {
+        colorList[i] = colors[getRandomInt()]; // Ligne [i] = color random
+        console.log("colorlist", i, colorList[i]);
+        i++;
     }
 }
-
-colorSuiteChooser(); // on lance la fonction pour générer la suite de couleur à deviner
-
-
 let y = 0;
-
-function displayColorSuit() {
+function displayColorList() {
     setTimeout(function () {
-        document.getElementById('colorSuite').style.background = "none";
-
+        document.getElementById('colorList').style.background = "none";
         y++;
-        if (y < colorSuit.length) {
-            displayColorSuit();
+        if (y < colorList.length) {
+            displayColorList();
         }
     }, 2000);
     setTimeout(function () {
-        document.getElementById('colorSuite').style.background = colorSuit[y];
-        console.log(colorSuit[y]);
-
+        document.getElementById('colorList').style.background = colorList[y];
+        // console.log(colorList[y]);
     }, 1000);
 }
-
-displayColorSuit();
-
+let userColorList = new Array(); // init tableau joueur
+let z = 0;
+function nextUserColorPicker(color) {
+    userColorList[z] = color;
+    console.log("userColorList", z, userColorList[z]);
+    z++;
+    compareLists();
+}
+let points = 0;
+function compareLists() {
+    if (arraysEqual(colorList, userColorList)) {
+        points++;
+        document.getElementById('points').innerHTML = points;
+    }
+    document.getElementById('colorListDisplay').innerHTML = "color list : " + colorList;
+    document.getElementById('userColorListDisplay').innerHTML = "user color list : " + userColorList;
+}
+function arraysEqual(arr1, arr2) {
+    // Check if the arrays have the same length
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    // Check if all elements in the arrays are equal
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
